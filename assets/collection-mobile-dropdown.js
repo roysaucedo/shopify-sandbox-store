170c225cad24bdedb2a897eaf970ee3864ba420c
Roy Saucedo <72239122+roysaucedo@users.noreply.github.com>
2022-07-06 16:30:14 -0700

    Assetes Folder

---
+++ assets/collection-mobile-dropdown.js
@@ -0,0 +1,6 @@
+function dropdownBtnClick() {
+    if(document.querySelector('#dropdown-btn').style.display = 'block') {
+        document.querySelector('#show-tabs-div').style.display = 'block';
+    } 
+    
+}
